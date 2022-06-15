1d0b538e643648032141e37f0cd011426b30abff
afiqhanafi <85728729+afiqhanafi@users.noreply.github.com>
2022-06-15 07:10:58 +0000

    Add files via upload

---
+++ whuut.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+
+import os
+import sys
+import optparse
+import tkMessageBox
+import csv
+
+from map15_2 import *
+
+# we need to import some python modules from the $SUMO_HOME/tools directory
+if 'SUMO_HOME' in os.environ:
+    tools = os.path.join(os.environ['SUMO_HOME'], 'tools')
+    sys.path.append(tools)
+else:
+    sys.exit("please declare environment variable 'SUMO_HOME'")
+
+
+from sumolib import checkBinary  # Checks for the binary in environ vars
+import traci
+
+
+def get_options():
+    opt_parser = optparse.OptionParser()
+    opt_parser.add_option("--nogui", action="store_true",
+                         default=False, help="run the commandline version of sumo")
+    options, args = opt_parser.parse_args()
+    return options
+
+
+class color:
+   PURPLE = '\033[95m'
+   CYAN = '\033[96m'
+   DARKCYAN = '\033[36m'
+   BLUE = '\033[94m'
+   GREEN = '\033[92m'
+   YELLOW = '\033[93m'
+   RED = '\033[91m'
+   BOLD = '\033[1m'
+   UNDERLINE = '\033[4m'
+   END = '\033[0m'
+    
+# contains TraCI control loop
+def run():
+    step = 0
+    
+    while traci.simulation.getMinExpectedNumber() > 0:
+        traci.simulationStep()
+        os.system('clear')
+        step += 1
+        
+    
+        veh = traci.vehicle.getIDList();
+
+        
+
+
+         
+      	print(veh)
+      	  		      
+    traci.close()
+    sys.stdout.flush()
+
+
+# main entry point
+if __name__ == "__main__":
+    options = get_options()
+
+    # check binary
+    if options.nogui:
+    
+    
+        sumoBinary = checkBinary('sumo')
+    else:
+        sumoBinary = checkBinary('sumo-gui')
+
+    # traci starts sumo as a subprocess and then this script connects and runs
+    traci.start([sumoBinary, "-c", "map15_3.sumo.cfg",
+                             "--tripinfo-output", "tripinfo.xml"])
+    run()
